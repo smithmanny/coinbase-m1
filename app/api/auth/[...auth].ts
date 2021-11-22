534d091b2a10a2564f68311aaa6f2593e9d80bb1
Shakhor <shakhorsmith@gmail.com>
2021-11-22 14:30:45 -0500

    Add railway url

--- app/api/auth/[...auth].ts
+++ app/api/auth/[...auth].ts
@@ -21,7 +21,7 @@ export default passportAuth((context) => ({
         account: 'all',
         clientID: process.env.COINBASE_CLIENT_ID,
         clientSecret: process.env.COINBASE_CLIENT_SECRET,
-        callbackURL: `${process.env.SITE_URL}/api/auth/coinbase/callback`,
+        callbackURL: `${process.env.RAILWAY_STATIC_URL || process.env.SITE_URL}/api/auth/coinbase/callback`,
         includeEmail: true,
       },
         async function (accessToken, refreshToken, profile: ProfileInterface, done) {
